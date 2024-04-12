1a54263bd2e23d882728c101419a69e9e48738c3
Nahid Ahmed <npahmed92@gmail.com>
2024-04-12 01:07:00 -0400

    add route to update category with id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,7 +27,7 @@ router.get('/:id', async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'Category no found with current id' });
+      res.status(404).json({ message: 'Category not found with current id' });
       return;
     }
 
@@ -47,8 +47,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'Category not found with current id' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
