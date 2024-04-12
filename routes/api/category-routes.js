81fef52bd121d881dcf3c1a4697ac18d429dc3bb
Nahid Ahmed <npahmed92@gmail.com>
2024-04-12 00:58:49 -0400

    add route to get category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,9 +17,24 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      // JOIN with Product
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'Category no found with current id' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
